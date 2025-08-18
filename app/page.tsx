cb274d1112b6d67d185509b5233c076ea6e46370
Kipyegon Lagat <kipyegongithub@gmail.com>
2025-08-18 23:33:29 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect, useRef, useCallback } from "reac"
+import { useState, useEffect, useRef, useCallback } from "react"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 
