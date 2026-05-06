52b8cf23f256326e0ff140371385bf7743a57bc3
Kipyegon Lagat <kipyegongithub@gmail.com>
2026-05-06 22:02:38 +0300

    Update accordion.tsx

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -8,7 +8,14 @@ import { cn } from "@/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
 
-const 
+const AccordionItem = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={cn("border-b", className)}
+    {...props}
   />
 ))
 AccordionItem.displayName = "AccordionItem"
