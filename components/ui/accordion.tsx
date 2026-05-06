eed8c779f6f71fcb9d9c00ea78a955b8810dc3e2
Kipyegon Lagat <kipyegongithub@gmail.com>
2026-05-06 22:02:11 +0300

    Refactor AccordionItem to use forwardRef

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -8,7 +8,12 @@ import { cn } from "@/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
 
-c
+const AccordionItem = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <AccordionPrimitive.Item
+    ref={ref}
     className={cn("border-b", className)}
     {...props}
   />
